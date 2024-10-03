93f4d84e3f487f691d0e78782bc829e9d55c8c6b
qzbing <qzb0212@126.com>
2024-10-03 11:33:12 +0800

    推断构造方法

---
+++ spring-qzb/src/main/java/org/springframework/qzb/server/B.java
@@ -0,0 +1,10 @@
+package org.springframework.qzb.server;
+
+/**
+ * B.
+ *
+ * @Author qzb0212@126.com
+ * @Date 2024-10-03 11:24:31
+ */
+public class B extends C {
+}
---
+++ spring-qzb/src/main/java/org/springframework/qzb/server/E.java
@@ -0,0 +1,10 @@
+package org.springframework.qzb.server;
+
+/**
+ * E.
+ *
+ * @Author qzb0212@126.com
+ * @Date 2024-10-03 11:30:15
+ */
+public interface E {
+}
